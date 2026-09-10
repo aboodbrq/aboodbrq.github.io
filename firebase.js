5bd88d3b718d8986d561ff950d3fea6beb4a8b23
aboodbrq <aboodbrq@gmail.com>
2026-09-11 02:54:56 +0300

    Add Firebase configuration and initialization script

---
+++ firebase.js
@@ -0,0 +1,15 @@
+<script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZg3k6l1HE-uMDVIzorSo4mTm6P2Is1MY",
+    authDomain: "abdullah-profile-9e0e6.firebaseapp.com",
+    projectId: "abdullah-profile-9e0e6"
+  };
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const auth = firebase.auth();
+</script>
