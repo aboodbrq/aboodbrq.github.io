d5235ba306deac19bde9d903245d21f4c2d7ebe0
aboodbrq <aboodbrq@gmail.com>
2026-09-11 04:06:54 +0300

    Refactor Firebase initialization in firebase.js
    Removed script tags and initialized Firebase directly with configuration.

--- firebase.js
+++ firebase.js
@@ -1,15 +1,9 @@
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
+// Firebase v8
+firebase.initializeApp({
+  apiKey: "AIzaSyAZg3k6l1HE-uMDVIzorSo4mTm6P2Is1MY",
+  authDomain: "abdullah-profile-9e0e6.firebaseapp.com",
+  projectId: "abdullah-profile-9e0e6"
+});
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyAZg3k6l1HE-uMDVIzorSo4mTm6P2Is1MY",
-    authDomain: "abdullah-profile-9e0e6.firebaseapp.com",
-    projectId: "abdullah-profile-9e0e6"
-  };
-
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-  const auth = firebase.auth();
-</script>
+const db = firebase.firestore();
+const auth = firebase.auth();
