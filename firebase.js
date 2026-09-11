6fe3d40c306690df0e1413a8ae2815fcc094f65d
aboodbrq <aboodbrq@gmail.com>
2026-09-11 04:16:21 +0300

    Initialize Firebase with configuration settings

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// Firebase v8
+firebase.initializeApp({
+  apiKey: "AIzaSyAZg3k6l1HE-uMDVIzorSo4mTm6P2Is1MY",
+  authDomain: "abdullah-profile-9e0e6.firebaseapp.com",
+  projectId: "abdullah-profile-9e0e6"
+});
+
+const db = firebase.firestore();
+const auth = firebase.auth();
